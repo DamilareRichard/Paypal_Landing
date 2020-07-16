03c75185d6c6622d6f28d2715be26306f914615e
Damilare Richard Olagbende <damilareshinaayo@gmail.com>
2020-07-16 03:34:17 +0100

    Paypal

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const rightHand = document.getElementById('rightHand');
+
+
+hamburger.addEventListener('click', () => {
+	rightHand.classList.toggle('show');
+
+
+
+
+});
+
+// document.getElementById('menu-bg').classList.toggle('change-bg');
